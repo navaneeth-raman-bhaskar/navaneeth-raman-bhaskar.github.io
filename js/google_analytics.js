d0e242694710525e73560ab614100e22e69425a1
navaneeth <navaneethbhasuram@gmail.com>
2022-05-10 00:30:29 +0530

    analytics tweaks changed v2.1.1

---
+++ js/google_analytics.js
@@ -0,0 +1,6 @@
+if(! window.location.host.includes('.test')){
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-D32039T5VP');
+}
